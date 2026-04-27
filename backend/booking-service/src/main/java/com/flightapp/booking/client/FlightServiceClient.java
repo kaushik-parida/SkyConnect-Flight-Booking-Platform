dca6a83f86cd07c528ff1439d66d84b6d0599786
kaushik-teksystems <kaushik.teksystems@gmail.com>
2026-04-27 11:35:56 +0530

    refactor(booking-service): replaced manual logger with @Slf4j annotation

--- backend/booking-service/src/main/java/com/flightapp/booking/client/FlightServiceClient.java
+++ backend/booking-service/src/main/java/com/flightapp/booking/client/FlightServiceClient.java
@@ -3,8 +3,6 @@ package com.flightapp.booking.client;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -13,10 +11,11 @@ import com.flightapp.booking.dto.external.FlightResponse;
 import com.flightapp.booking.exception.FlightNotFoundException;
 import com.flightapp.booking.exception.FlightServiceUnavailableException;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class FlightServiceClient {
-
-	private static final Logger log = LoggerFactory.getLogger(FlightServiceClient.class);
 
 	private final WebClient webClient;
 
