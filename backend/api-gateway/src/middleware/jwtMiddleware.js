d8691cdbc3e21afff8e9987bf83c3e5976470580
Sujan <gamingxdominic@gmail.com>
2026-04-28 11:31:03 +0530

    Fixed Sonar issues: moved JWT secret to env and improved optional chaining

--- backend/api-gateway/src/middleware/jwtMiddleware.js
+++ backend/api-gateway/src/middleware/jwtMiddleware.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = "mySuperSecretKey12345678901234567890";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const jwtMiddleware = (req, res, next) => {
 
-    const authHeader = req.headers['authorization'];
+   const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+if (!authHeader?.startsWith('Bearer ')) {
         return res.status(401).json({ message: "Missing or invalid token" });
     }
 
