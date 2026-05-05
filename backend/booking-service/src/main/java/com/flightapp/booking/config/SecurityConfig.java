d52f9ac1c2bd1714e66220083976f5e09cabd065
kaushik-teksystems <kaushik.teksystems@gmail.com>
2026-05-05 16:34:57 +0530

    feat(booking-service): Added security integration through auth-service

---
+++ backend/booking-service/src/main/java/com/flightapp/booking/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.flightapp.booking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final GatewayHeaderFilter gatewayHeaderFilter;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(
+						auth -> auth.requestMatchers("/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+								.requestMatchers("/api/v1.0/flight/booking/admin/**").hasRole("ADMIN").anyRequest()
+								.authenticated())
+				.addFilterBefore(gatewayHeaderFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+}
