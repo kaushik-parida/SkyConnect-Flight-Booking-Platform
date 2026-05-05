5b0a53331c9360f5e9d0de59988d8579373220a8
kaushik-teksystems <kaushik.teksystems@gmail.com>
2026-05-05 17:35:57 +0530

    fixed sonarqube code smells

--- backend/booking-service/src/main/java/com/flightapp/booking/config/SecurityConfig.java
+++ backend/booking-service/src/main/java/com/flightapp/booking/config/SecurityConfig.java
@@ -2,33 +2,35 @@ package com.flightapp.booking.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
 	private final GatewayHeaderFilter gatewayHeaderFilter;
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(
-						auth -> auth.requestMatchers("/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-								.requestMatchers("/api/v1.0/flight/booking/admin/**").hasRole("ADMIN").anyRequest()
-								.authenticated())
-				.addFilterBefore(gatewayHeaderFilter, UsernamePasswordAuthenticationFilter.class);
+	public SecurityConfig(GatewayHeaderFilter gatewayHeaderFilter) {
+		this.gatewayHeaderFilter = gatewayHeaderFilter;
+	}
 
-		return http.build();
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) {
+		try {
+			http.csrf(csrf -> csrf.disable())
+					.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+					.authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1.0/flight/booking/admin/**")
+							.hasRole("ADMIN").requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+							.permitAll().anyRequest().authenticated())
+					.addFilterBefore(gatewayHeaderFilter, UsernamePasswordAuthenticationFilter.class);
+
+			return http.build();
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to configure security filter chain", e);
+		}
 	}
 }
